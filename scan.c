acac27cdca180dee28fd30c05fb59b3d5c437557
ladymindhunter <104469296+ladymindhunter@users.noreply.github.com>
2022-05-01 19:40:04 +0300

    Create scan.c

---
+++ scan.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+    int main() {
+        int a,b;
+
+            printf("iki tam sayı giriniz: \n");
+            scanf("%d %d", &a, &b);
+
+            printf("Sayıların toplamı: %d\n", a+b);
+            
+    }
